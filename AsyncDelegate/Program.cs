012e2f723678766525c76a1aeee8c88705bf4c36
Inchensenk <aab8117@gmail.com>
2022-11-16 17:31:22 +0300

    стр 693

---
+++ AsyncDelegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+/*стр 693*/
+namespace AsyncDelegate
+{
+    internal class Program
+    {
+        public delegate int BinaryOp(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Async Delegate Invocation *****");
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Main() invoked on thread { 0}.",Thread.CurrentThread.ManagedThreadId);
+
+            // Вызвать Add() во вторичном потоке.
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult ar = b.BeginInvoke(10, 10, null, null);
+
+            // Выполнить другую работу в первичном потоке...
+            Console.WriteLine("Doing more work in Main () ! ");
+
+            // По готовности получить результат выполнения метода Add().
+            int answer = b.EndInvoke(ar);
+            Console.WriteLine("10 + 10 is {0}.", answer);
+            Console.ReadLine();
+
+
+        }
+
+        static int Add(int x, int y)
+        {
+            // Вывести идентификатор выполняющегося потока.
+            Console.WriteLine("Add () invoked on thread {0}.",
+            Thread.CurrentThread.ManagedThreadId);
+            // Сделать паузу для моделирования длительной операции.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
