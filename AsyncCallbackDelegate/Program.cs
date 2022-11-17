129b9ee2d9b85c31fd8bed374dc89358f47c0f45
Inchensenk <aab8117@gmail.com>
2022-11-17 14:55:07 +0300

    Роль делегата AsyncCallback

--- AsyncCallbackDelegate/Program.cs
+++ AsyncCallbackDelegate/Program.cs
@@ -11,6 +11,7 @@ namespace AsyncCallbackDelegate
             Console.WriteLine("Main() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
             BinaryOp b = new BinaryOp(Add);
             IAsyncResult ar = b.BeginInvoke(10, 10, new AsyncCallback(AddComplete), null);
+
             // Предположим, что здесь делается какая-то другая работа...
             while (!isDone)
             {
@@ -21,8 +22,7 @@ namespace AsyncCallbackDelegate
         }
         static int Add(int x, int y)
         {
-            Console.WriteLine("Add() invoked on thread {0}.",
-            Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Add() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
             Thread.Sleep(5000);
             return x + y;
         }
@@ -30,10 +30,33 @@ namespace AsyncCallbackDelegate
 
         static void AddComplete(IAsyncResult iar)
         {
-            Console.WriteLine("AddComplete() invoked on thread {0}."z
-            Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("AddComplete() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
             Console.WriteLine("Your addition is complete");
             isDone = true;
         }
     }
 }
+
+/*На заметку! Строго говоря, использование булевской переменной в данном примере не является
+безопасным в отношении потоков, поскольку к ее значению имеют доступ два разных потока.
+В текущем примере подобное допустимо; тем не менее, запомните в качестве очень важного
+эмпирического правила: вы должны обеспечивать блокировку данных, разделяемых между несколькими потоками. Вы увидите, как это делается, далее в главе.*/
+
+/*И снова статический метод AddComplete () будет вызван делегатом AsyncCallback
+после завершения метода Add (). Запустив программу, можно убедиться, что именно
+вторичный поток вызывает AddComplete ():
+
+***** AsyncCallbackDelegate Example *****
+Main() invoked on thread 1.
+Add () invoked on thread 3.
+Working....
+Working....
+Working....
+Working....
+Working....
+AddComplete () invoked on thread 3.
+Your addition is complete
+
+ Подобно другим примерам, приведенным в настоящей главе, вывод может несколько отличаться. На самом деле вполне возможно, что после завершения работы метода
+AddComplete () появится только одно финальное сообщение Working.... Это просто побочный эффект односекундной задержки в Main ().
+ */
